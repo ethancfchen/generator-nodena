0e6eeb2c4c51b65c8d4be8572cbbf9a9b8eead25
ethancfchen <ethan.cfchen@gmail.com>
2017-09-03 22:34:21 +0800

    Fix stream error

--- generators/app/templates/gulp-tasks/version.js
+++ generators/app/templates/gulp-tasks/version.js
@@ -1,8 +1,52 @@
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const $ = require('gulp-load-plugins')();
 
 const setup = require('setup/setup');
+
+function gitCommitAndTag() {
+  gitAdd()
+    .then(gitCommit)
+    .then(gitTag);
+}
+
+function gitAdd() {
+  const targetFile = setup.assets.manifest;
+  const command = ['add', targetFile].join(' ');
+  return new Promise((resolve, reject) => {
+    $.git.exec({args: command}, (error) => {
+      if (error) {
+        reject(error);
+      }
+      resolve();
+    });
+  });
+}
+
+function gitCommit() {
+  const version = setup.getVersion();
+  const command = ['commit', '-m', version].join(' ');
+  return new Promise((resolve, reject) => {
+    $.git.exec({args: command}, (error) => {
+      if (error) {
+        reject(error);
+      }
+      resolve();
+    });
+  });
+}
+
+function gitTag() {
+  const version = setup.getVersion();
+  const command = ['tag', '-a', version, '-m', version].join(' ');
+  return new Promise((resolve, reject) => {
+    $.git.exec({args: command}, (error) => {
+      if (error) {
+        reject(error);
+      }
+      resolve();
+    });
+  });
+}
 
 module.exports = function() {
   const assets = setup.assets;
@@ -12,12 +56,5 @@ module.exports = function() {
   return gulp.src(assets.manifest)
     .pipe($.bump(optionsBump))
     .pipe(gulp.dest('./'))
-    .on('end', (event) => {
-      gutil.log(setup.getVersion());
-      gulp.src(assets.manifest)
-        .pipe($.git.commit(setup.getVersion()))
-        .on('data', () => {
-          $.git.tag(setup.getVersion());
-        });
-    });
+    .on('end', gitCommitAndTag);
 };
