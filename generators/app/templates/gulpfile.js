6e5ad5551bd5f8426fa6324e2b88fe6a74edab2f
ethancfchen <ethan.cfchen@gmail.com>
2017-09-28 23:20:07 +0800

    Upgrade Gulp to 4.0

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -1,17 +1,15 @@
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 
-const fs = require('fs');
-const path = require('path');
+const GulpRegistry = require('undertaker-forward-reference');
 
-const PATH_TASK_LOADER = 'gulptasks.js';
+const browserSync = require('browser-sync').create();
 
 require('rootpath')();
 
-if (fs.existsSync(path.resolve(__dirname, PATH_TASK_LOADER))) {
-  require(path.resolve(__dirname, PATH_TASK_LOADER))();
-} else {
-  $.taskLoader();
-}
+gulp.registry(new GulpRegistry());
+$.loadAllTasks({
+  browserSync,
+});
 
-gulp.task('default', ['build']);
+gulp.task('default', gulp.series('build'));
