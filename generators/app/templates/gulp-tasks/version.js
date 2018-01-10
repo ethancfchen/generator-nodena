f1945aff94b4c850919738a135225682e28cf3b1
Ethan Chen <ethan.cfchen@gmail.com>
2018-01-10 18:26:57 +0800

    Refactoring

--- generators/app/templates/gulp-tasks/version.js
+++ generators/app/templates/gulp-tasks/version.js
@@ -1,16 +1,53 @@
+const fs = require('fs');
+const semver = require('semver');
+const chalk = require('chalk');
 const gulp = require('gulp');
+const gutil = require('gulp-util'); // @Deprecated
 const $ = require('gulp-load-plugins')();
 
-const setup = require('setup/setup');
+const NODE_ENV = process.env.NODE_ENV;
+const NODE_APP_INSTANCE = process.env.NODE_APP_INSTANCE;
+const ASSETS = {
+  manifest: 'package.json',
+};
 
-function gitCommitAndTag() {
-  gitAdd()
-    .then(gitCommit)
-    .then(gitTag);
+const argv = require('yargs').option({
+  n: {
+    alias: 'new',
+    type: 'string',
+    nargs: 1,
+  },
+  preid: {
+    type: 'string',
+    nargs: 1,
+    default:
+      (NODE_ENV ? NODE_ENV : '') +
+      (NODE_APP_INSTANCE ? '-' + NODE_APP_INSTANCE : ''),
+  },
+}).argv;
+
+function getVersion() {
+  const fileContent = fs.readFileSync(ASSETS.manifest, 'utf8');
+  const manifest = JSON.parse(fileContent);
+  return 'v' + manifest.version;
+}
+
+function getNewVersion() {
+  const input = argv.new;
+  const postfix = argv.preid;
+  const current = getVersion();
+  let target = input;
+
+  if (semver.valid(input)) {
+    target = input;
+  } else {
+    target = semver.inc(current, input, postfix);
+  }
+  return target;
 }
 
 function gitAdd() {
-  const targetFile = setup.assets.manifest;
+  const targetFile = ASSETS.manifest;
   const command = ['add', targetFile].join(' ');
   return new Promise((resolve, reject) => {
     $.git.exec({args: command}, (error) => {
@@ -23,7 +60,7 @@ function gitAdd() {
 }
 
 function gitCommit() {
-  const version = setup.getVersion();
+  const version = getVersion();
   const command = ['commit', '-m', version].join(' ');
   return new Promise((resolve, reject) => {
     $.git.exec({args: command}, (error) => {
@@ -36,7 +73,7 @@ function gitCommit() {
 }
 
 function gitTag() {
-  const version = setup.getVersion();
+  const version = getVersion();
   const command = ['tag', '-a', version, '-m', version].join(' ');
   return new Promise((resolve, reject) => {
     $.git.exec({args: command}, (error) => {
@@ -49,12 +86,21 @@ function gitTag() {
 }
 
 module.exports = function() {
-  const assets = setup.assets;
+  const version = getVersion();
+  const isNew = argv.new;
 
-  const optionsBump = setup.plugins.gulpBump;
-
-  return gulp.src(assets.manifest)
-    .pipe($.bump(optionsBump))
+  return gulp.src(ASSETS.manifest)
+    .pipe(isNew ? $.bump({
+      version: getNewVersion(),
+    }) : gutil.noop())
     .pipe(gulp.dest('./'))
-    .on('end', gitCommitAndTag);
+    .on('end', () => {
+      if (isNew) {
+        gitAdd()
+          .then(gitCommit)
+          .then(gitTag);
+      } else {
+        gutil.log(`Package version ${chalk.magenta(version)}`);
+      }
+    });
 };
--- generators/app/templates/setup/argv.js
+++ generators/app/templates/setup/argv.js
@@ -1,6 +1,3 @@
-const NODE_ENV = process.env.NODE_ENV;
-const NODE_APP_INSTANCE = process.env.NODE_APP_INSTANCE;
-
 module.exports = require('yargs').option({
   b: {
     alias: 'verbose',
@@ -9,19 +6,5 @@ module.exports = require('yargs').option({
     alias: 'port',
     type: 'number',
     nargs: 1,
-  },
-  n: {
-    alias: 'new-version',
-    type: 'string',
-    nargs: 1,
-    default: 'prerelease',
-  },
-
-  preid: {
-    type: 'string',
-    nargs: 1,
-    default:
-      (NODE_ENV ? NODE_ENV : '') +
-      (NODE_APP_INSTANCE ? '-' + NODE_APP_INSTANCE : ''),
   },
 }).argv;
